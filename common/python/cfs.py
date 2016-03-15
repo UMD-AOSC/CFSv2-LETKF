7b2f21e6e3315a49d5f218b0fd5b246db92d15ba
Travis Sluka <tsluka@atmos.umd.edu>
2016-03-15 18:06:08 -0400

    python function to get ocean grid x/y from a lat/lon

--- common/python/cfs.py
+++ common/python/cfs.py
@@ -1,7 +1,12 @@
 ################################################################################
 ################################################################################
 import sys
+import os
+import netCDF4 as nc
+import numpy as np
+import scipy.spatial
 
+_root = os.getenv("CFS_LETKF_ROOT")
 
 ## make sure this module is not being run from the command line
 if __name__ == '__main__':
@@ -23,3 +28,42 @@ def getAtmRes(t_res):
         1148 : ('2304', '1152')}
     assert(t_res in res)
     return res[t_res]
+
+## gets the x/y coordinate on the ocean grid given the
+## latitude and longitude
+_ocnGrd_KDTree = None
+_ocnGrd_lons = None
+_ocnGrd_lats = None
+
+def getOcnXY(lon, lat):
+    global _ocnGrd_KDTree
+    global _ocnGrd_lons
+    global _ocnGrd_lats
+    global _ocnGrd_xs
+    global _ocnGrd_ys
+    global _ocnGrd_lon_bounds
+    if _ocnGrd_KDTree == None:
+        print 'Loading ocean grid into KD Tree...'
+        ncdat = nc.Dataset(_root+"/support/fix/fix_om/grid_spec_05.nc.T62")
+        _ocnGrd_lons = ncdat.variables["x_T"][:]
+        _ocnGrd_lats = ncdat.variables["y_T"][:]
+        _ocnGrd_lon_bounds = ( _ocnGrd_lons[0][0],_ocnGrd_lons[0][-1])
+        _ocnGrd_KDTree = scipy.spatial.KDTree(zip(_ocnGrd_lons.reshape(-1),
+                                                  _ocnGrd_lats.reshape(-1)))
+        xs, ys = np.meshgrid(np.arange(0,_ocnGrd_lons.shape[1]),
+                             np.arange(0,_ocnGrd_lons.shape[0]))
+        _ocnGrd_xs = xs.reshape(-1)
+        _ocnGrd_ys = ys.reshape(-1)
+        
+        ncdat.close()
+
+    if lon > _ocnGrd_lon_bounds[1]:
+        lon -= 360
+    if lon < _ocnGrd_lon_bounds[0]:
+        lon += 360
+        
+    res = _ocnGrd_KDTree.query( (lon,lat) )
+    y = _ocnGrd_ys[res[1]]
+    x = _ocnGrd_xs[res[1]]
+    return x,y
+    
